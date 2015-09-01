623f30f7e0daa4498e61e17675b48989b2468f50
rudak <roiduboul@gmail.com>
2015-09-01 22:10:15 +0200

    add files

---
+++ Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Rudak\FlBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('RudakFlBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ RudakFlBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Rudak\FlBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class RudakFlBundle extends Bundle
+{
+}
